48c3a6c9a8aabfe2d2069c3c3571ebdc8b298750
pavithramuddu93 <pavithramuddu93>
2022-07-05 10:51:48 +0530

    UC2 added

---
+++ src/com/blz/StockInfo1.java
@@ -0,0 +1,49 @@
+package com.blz;
+
+
+public class StockInfo1 {
+    private String stockName;
+    private String numberOfShare;
+    private String sharePrice;
+
+    // constructor
+    public StockInfo1(String stockName, String numberOfShare, String sharePrice) {
+        super();
+        this.stockName = stockName;
+        this.numberOfShare = numberOfShare;
+        this.sharePrice = sharePrice;
+    }
+
+    // getter and setter
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public String getNumberOfShare() {
+        return numberOfShare;
+    }
+
+    public void setNumberOfShare(String numberOfShare) {
+        this.numberOfShare = numberOfShare;
+    }
+
+    public String getSharePrice() {
+        return sharePrice;
+    }
+
+    public void setSharePrice(String sharePrice) {
+        this.sharePrice = sharePrice;
+    }
+
+    // print output in format
+    @Override
+    public String toString() {
+        return "StockInfo [stockName=" + stockName + ", /nnumberOfShare=" + numberOfShare + ", \nsharePrice="
+                + sharePrice + "]";
+    }
+
+}
